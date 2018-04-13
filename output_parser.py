1c21a4764d116a2928796cf03f8d1c58f6ee435e
Jieyang Chen <cjy7117@gmail.com>
2018-04-12 18:42:16 -0700

    Update output_parser.py

--- output_parser.py
+++ output_parser.py
@@ -2,9 +2,7 @@
 import getpass
 print(getpass.getuser())
 print(getpass.getuser())
-print(getpass.getuser())
-print(getpass.getuser())
-print(getpass.getuser())
+
 import glob
 output_file = open('bee_scalability_test_parsed.output','w')
 output_file.write("Num. of nodes, Proc. per node, Execution time\n")
